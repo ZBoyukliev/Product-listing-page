0d20539b755bee1fafa1d659c115681a93cc62a5
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-07-27 14:18:42 +0300

    create header component

--- plp-app/src/App.tsx
+++ plp-app/src/App.tsx
@@ -1,11 +1,13 @@
 
 import './App.scss';
+import Header from './components/header/Header';
 
 function App() {
 
 
   return (
     <>
+    <Header />
     <h1>it works</h1>
      <button className="btn btn-primary">Submit</button>
     </>
