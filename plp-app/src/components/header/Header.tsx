b22250765b061c7d6e0efba4b6b58eeb7fb7c5f0
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-07-29 23:44:43 +0300

    change css in navbar

--- plp-app/src/components/header/Header.tsx
+++ plp-app/src/components/header/Header.tsx
@@ -8,13 +8,13 @@ const Header: FunctionComponent   = () => {
     return (
         <>
             <nav className={`${styles['nav-main']} navbar navbar-expand-lg fixed-top`}>
-                <div className={`${styles['nav-container']} container `}>
+                <div className={`${styles['nav-container']} container nav-bar `}>
                 <a className="navbar-brand" href="#"><img src='/images/logoB1.png'/></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className={`${styles['nav-div-ul']} collapse navbar-collapse `} id="navbarSupportedContent">
-                        <ul className={`${styles['nav-ul']} navbar-nav me-auto mb-2 mb-lg-0 `}>
+                    <div className={`${styles['nav-div-ul']}`} id="navbarSupportedContent">
+                        <ul className={`${styles['nav-ul']} navbar-nav me-auto mb-2 mb-lg-0 justify-content-around`}>
                             <li className={`${styles['nav-ul-li']} nav-item`}>
                                 <Link to="/" className="nav-link active" aria-current="page" >Home</Link>
                             </li>
