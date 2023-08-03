dd49876dd5b6b16050ee61ed6b8ff71cf9ec6e7d
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-08-03 16:06:32 +0300

    add more colors to filter

--- plp-app/src/components/main/filter/Filter.tsx
+++ plp-app/src/components/main/filter/Filter.tsx
@@ -1,41 +1,57 @@
 
 import React, { FunctionComponent, useContext } from 'react';
-import './Filter.scss';
+import styles from './Filter.module.scss';
 import { ProductContext } from '../../../context/products';
 import { ProductContextType } from '../../../context/models/ContextModel';
 
 const FilterComponent: FunctionComponent = () => {
   const productContext = useContext<ProductContextType | null>(ProductContext);
-  
-
   if (!productContext) {
     return <div>Loading...</div>;
   }
 
-  const {handleSelectedColorsChange, handleMaximumPriceSelect} = productContext;
+  const { handleSelectedColorsChange, handleMaximumPriceSelect } = productContext;
 
   return (
-    <div className='filter-container col-3 m-4'>
+    <div className={`${styles['filter-main']} filter-container col-3 m-4`}>
       <h5>Filter By:</h5>
       <div className="mb-3">
         <label htmlFor="colorFilter" className="form-label">Color:</label>
         <div>
-          <div className="form-check">
-            <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSelectedColorsChange(e)}
-             type="checkbox"
-              value='red' 
-              className="form-check-input" 
-              id="red" 
-              />
-            <label className="form-check-label" htmlFor="red">Red</label>
-          </div>
+
           <div className="form-check">
             <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSelectedColorsChange(e)} type="checkbox" value='black' className="form-check-input" id="black" />
-            <label className="form-check-label" htmlFor="black">Black</label>
+            <label className={`${styles['color-black']} form-check-label`} htmlFor="black">Black</label>
+          </div>
+          <div className="form-check">
+            <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSelectedColorsChange(e)} value='white' type="checkbox" className="form-check-input" id="blue" />
+            <label className={`${styles['color-white']} form-check-label`} htmlFor="blue">White</label>
           </div>
           <div className="form-check">
             <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSelectedColorsChange(e)} value='blue' type="checkbox" className="form-check-input" id="blue" />
-            <label className="form-check-label" htmlFor="blue">Blue</label>
+            <label className={`${styles['color-blue']} form-check-label`} htmlFor="blue">Blue</label>
+          </div>
+          <div className="form-check">
+            <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSelectedColorsChange(e)}
+              type="checkbox"
+              value='red'
+              className="form-check-input"
+              id="red"
+            />
+            <label className={`${styles['color-red']} form-check-label`} htmlFor="red">Red</label>
+          </div>
+    
+          <div className="form-check">
+            <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSelectedColorsChange(e)} value='brown' type="checkbox" className="form-check-input" id="blue" />
+            <label className={`${styles['color-brown']} form-check-label`} htmlFor="blue">Brown</label>
+          </div>
+          <div className="form-check">
+            <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSelectedColorsChange(e)} value='pink' type="checkbox" className="form-check-input" id="blue" />
+            <label className={`${styles['color-pink']} form-check-label`} htmlFor="blue">Pink</label>
+          </div>
+          <div className="form-check">
+            <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSelectedColorsChange(e)} value='gray' type="checkbox" className="form-check-input" id="blue" />
+            <label className={`${styles['color-gray']} form-check-label`} htmlFor="blue">Gray</label>
           </div>
         </div>
       </div>
@@ -45,7 +61,7 @@ const FilterComponent: FunctionComponent = () => {
         <div>
           <div className="form-check">
             <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleMaximumPriceSelect(e)} type="checkbox" className="form-check-input" value={50} id="priceLow" />
-            <label className="form-check-label"  htmlFor="priceLow">Low</label>
+            <label className="form-check-label" htmlFor="priceLow">Low</label>
           </div>
           <div className="form-check">
             <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleMaximumPriceSelect(e)} type="checkbox" className="form-check-input" value={200} id="priceMedium" />
