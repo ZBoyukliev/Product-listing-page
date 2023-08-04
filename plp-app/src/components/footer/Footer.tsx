2fa4ced02e4e856e1b2a91755b779969a581873b
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-08-04 11:33:19 +0300

    add icons in footer

--- plp-app/src/components/footer/Footer.tsx
+++ plp-app/src/components/footer/Footer.tsx
@@ -1,12 +1,11 @@
 import { FunctionComponent } from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import styles from  './Footer.module.scss';
+import { IconDefinition } from '@fortawesome/fontawesome-common-types';
+
 
 const Footer: FunctionComponent = () => {
-  
-  // library.add(faFacebook ); // Add any other Font Awesome icons you need here
 
   return (
     <footer className={`${styles['foot-main']} text-center text-lg-start`}>
@@ -20,16 +19,22 @@ const Footer: FunctionComponent = () => {
           </div>
 
           <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-            <h5 className="text-uppercase">Links</h5>
+            <h5 className="text-uppercase">FOLLOW US</h5>
             <ul className="list-unstyled">
               <li>
-                <a href="https://www.facebook.com/">facebook</a>
+              <a href="https://www.facebook.com/">
+                    <FontAwesomeIcon icon={faFacebook as IconDefinition} />
+                  </a>
               </li>
               <li>
-                <a href="https://twitter.com/">twitter</a>
+              <a href="https://twitter.com/">
+                    <FontAwesomeIcon icon={faTwitter as IconDefinition} />
+                  </a>
               </li>
               <li>
-                <a href="https://www.instagram.com/">instagram</a>
+              <a href="https://www.instagram.com/">
+              <FontAwesomeIcon icon={faInstagram as IconDefinition} />
+            </a>
               </li>
             </ul>
           </div>
@@ -52,7 +57,7 @@ const Footer: FunctionComponent = () => {
       </div>
 
       <div className="text-center p-3 bg-dark text-white">
-        © {new Date().getFullYear()} Your App Name. All rights reserved.
+        © {new Date().getFullYear()} Product Listing Page App. All rights reserved.
       </div>
     </footer>
   );
