7c107c4dcd3d45ee795c800669e8524acaea2e89
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2023-11-25 21:33:34 +0200

    Update priceFiilteredProduct.ts

--- plp-app/src/context/helpers/priceFiilteredProduct.ts
+++ plp-app/src/context/helpers/priceFiilteredProduct.ts
@@ -2,6 +2,7 @@ import Product from "../../types/Product";
 import { Filter } from "../models/filter";
 
 export const setPriceFilteredProducts = (data: Product[], filters: Filter) => {
+  
   let items: Product[];
   if (filters.price === 99.99) {
     items = data.filter((product) => (product.discountPrice || product.price) <= filters.price);
