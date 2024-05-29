c5910b02c09b905b770fa74f3eaaaee0ba119937
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2024-05-29 17:16:15 +0300

    Update Sorter.tsx

--- plp-app/src/components/main/sort/Sorter.tsx
+++ plp-app/src/components/main/sort/Sorter.tsx
@@ -33,29 +33,3 @@ const SorterComponent: FunctionComponent = () => {
 };
 
 export default SorterComponent;
-
-
-
-
-
-
-// import { FunctionComponent } from 'react';
-// import './Sorter.scss';
-// import Product from '../../../types/Product';
-
-// const SorterComponent: FunctionComponent = () => {
-
-//     return (
-//         <div className="container sorter col-4">
-//             {products.map(product => (
-//                 <div key={product._id} className="row">
-//                     <div className="col-sm-6 col-md-4 col-lg-3">
-                       
-//                     </div>
-//                 </div>
-//             ))}
-//         </div>
-//     );
-// };
-
-// export default SorterComponent;
