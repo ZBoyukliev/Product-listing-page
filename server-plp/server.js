9b1af10668eb7e3c93e9ca26e636667f4aa6fa67
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2025-07-08 13:41:40 +0300

    Update server.js

--- server-plp/server.js
+++ server-plp/server.js
@@ -1446,7 +1446,7 @@
                 "description": "Shoe description for the pink shoes",
                 "imageUrl": "/images/shoes/shoes-pink.png",
                 "_createdOn": 2617194128606,
-                "price": 121.99,
+                "price": 129.99,
                 "rating": 5,
             },
             "sh436770-76c5-40e2-b231-77409eda7a07": {
@@ -1458,7 +1458,7 @@
                 "description": "Shoe description for the Barbie shoes",
                 "imageUrl": "/images/shoes/shoes-pink.png",
                 "_createdOn": 2617194128607,
-                "price": 109.99,
+                "price": 119.99,
                 "rating": 4,
             },
             
@@ -1471,7 +1471,7 @@
                 "description": "Shoe description for the Barbie shoes",
                 "imageUrl": "/images/shoes/shoes-pink.png",
                 "_createdOn": 2617194128607,
-                "price": 119.99,
+                "price": 115.99,
                 "rating": 5,
             },
 
